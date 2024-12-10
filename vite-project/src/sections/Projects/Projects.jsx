a7afbe84c5d2792d24c6e1904174399e609b1898
Gianna DAconti <themeanmememan@gmail.com>
2024-12-09 21:41:07 -0500

    added two more project cards

--- vite-project/src/sections/Projects/Projects.jsx
+++ vite-project/src/sections/Projects/Projects.jsx
@@ -4,6 +4,8 @@ import CarLogo from '../../assets/CarLogo.png'
 import PuppyLogo from '../../assets/PuppyLogo.png'
 import QuizLogo from '../../assets/QuizLogo.png'
 import WeatherLogo from '../../assets/WeatherLogo.png'
+import CandidateLookupLogo from '../../assets/CandidateLookupLogo.png'
+import KanbanBoard from '../../assets/KanbanBoard.png'
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -34,6 +36,18 @@ function Projects() {
           link="https://github.com/CodeCourtAce/WeatherTrackerApp.git"
           h3="Weather Tracker" 
           p="This project is a weather tracker that allows users to search for the weather in their area"
+        />
+        <ProjectCard 
+          src={CandidateLookupLogo} 
+          link="https://github.com/CodeCourtAce/CandidateLookup.git"
+          h3="Candidate Lookup" 
+          p="This project is a candidate lookup that allows users to search for candidates via GitHub API"
+        />
+        <ProjectCard 
+          src={KanbanBoard} 
+          link="https://github.com/CodeCourtAce/KKB.git"
+          h3="Kanban Board" 
+          p="This project is a Kanban Board that allows users to create and manage tasks" 
         />
       </div>
     </section>
